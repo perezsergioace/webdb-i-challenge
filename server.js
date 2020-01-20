e054268421bd917187d936ea50ef88f84994e938
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-20 00:07:52 -0500

    MVP

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// const db = require('./data/dbConfig.js');
+const accountRouter = require('./accounts/accountRouter');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter);
+
+server.get('/', (req, res) => {
+    res.send('This is working')
+})
 
 module.exports = server;
